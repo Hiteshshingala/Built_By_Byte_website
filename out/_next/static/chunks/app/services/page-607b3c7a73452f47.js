f7fb72639445203ad92e895999093cf4b0de3e85
HiteshShingalaServiceUp <hitesh-neebal@serviceup.com>
2026-02-21 22:57:07 +0530

    remove version 1

--- out/_next/static/chunks/app/services/page-607b3c7a73452f47.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469],{7499:function(e,n,s){Promise.resolve().then(s.bind(s,2580)),Promise.resolve().then(s.bind(s,1948)),Promise.resolve().then(s.bind(s,4926)),Promise.resolve().then(s.bind(s,6883))}},function(e){e.O(0,[972,423,455,971,117,744],function(){return e(e.s=7499)}),_N_E=e.O()}]);
